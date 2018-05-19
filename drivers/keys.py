baa1ab5d19ce3edae7852123803f7be720266b99
Maciek <ptasior3@gazeta.pl>
2018-05-20 00:49:50 +1000

    Button actions do not require polling

--- drivers/keys.py
+++ drivers/keys.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+import pigpio
 import time
 
 KEY_UP_PIN          = 6
@@ -9,8 +9,6 @@ KEY_PRESS_PIN       = 13
 KEY1_PIN            = 21
 KEY2_PIN            = 20
 KEY3_PIN            = 16
-
-REPEAT_EVERY = 0.3
 
 KEYS = {
         KEY_UP_PIN: 'UP',
@@ -25,72 +23,36 @@ KEYS = {
 
 
 class Keys:
-    def __init__(self, onDown, onRepeat=None, onUp=None):
+    def __init__(self, onDown, onUp=None):
         self.onDown = onDown
-        self.onRepeat = onRepeat
         self.onUp = onUp
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(KEY_UP_PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(KEY_DOWN_PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(KEY_LEFT_PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(KEY_RIGHT_PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(KEY_PRESS_PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(KEY1_PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(KEY2_PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(KEY3_PIN, GPIO.IN, GPIO.PUD_UP)
+        self.cb = []
 
-        self.state = {}
-        for k, v in KEYS.items():
-            self.state[k] = [False, False, True, None]
+        self.pi = pigpio.pi()
 
+        if not self.pi.connected:
+           raise Exception("Can not connect to pigpio")
+
+        for k in KEYS:
+            self.cb.append(self.pi.callback(k, pigpio.EITHER_EDGE, self.callback))
+
+        # c.cancel()
+        # pi.stop()
 
 
-    def start(self):
-        while True:
-            self.poll()
-
-
-
-    def poll(self):
-        for k, v in KEYS.items():
-            if GPIO.input(k) == 0:
-                self.state[k][0] = True
-                self.down(k)
-            else:
-                self.state[k][0] = False
-        self.up()
-
-
-
-    def down(self, key):
-        if not self.state[key][1]:
-            self.onDown(KEYS[key])
-            self.state[key] = [True, True, False, time.time()]
-
-
-
-    def up(self):
-        for k,v in KEYS.items():
-            if not self.state[k][0] and not self.state[k][2]:
-                if self.onUp: self.onUp(v)
-                self.state[k] = [False, False, True, None]
-            elif self.state[k][0] and self.state[k][3] < time.time()-REPEAT_EVERY:
-                if self.onRepeat: self.onRepeat(v)
-                self.state[k][3] = time.time()
-
+    def callback(self, gpio, level, tick):
+        if level: self.onDown(gpio)
+        else:     self.onUp(gpio)
 
 
 if __name__ == '__main__':
     def down(key):
-        print('Pressed: '+key)
-
-    def repeated(key):
-        print('Repeated: '+key)
+        print('Pressed: '+KEYS[key])
 
     def up(key):
-        print('Released: '+key)
+        print('Released: '+KEYS[key])
 
-    keys = Keys(down, repeated, up)
-    keys.start()
+    keys = Keys(down, up)
+    time.sleep(60)
 
